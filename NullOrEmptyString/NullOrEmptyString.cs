260782f07f3fe5024e17c6c10cda87b8270bab44
duxuso <duxuso@gmail.com>
2015-08-19 09:40:12 +1000

    Commint #2 NullOrEmptySting

--- NullOrEmptyString/NullOrEmptyString.cs
+++ NullOrEmptyString/NullOrEmptyString.cs
@@ -2,57 +2,74 @@
 
 namespace NullOrEmptyString
 {
+    /// <summary>
+    /// Class 'NullOrEmptyStrings' 
+    /// </summary>
     public class NullOrEmptyStrings
     {
         /// <summary>
-        /// find a string is null, empty or not
+        /// Finding if a string is null, empty or not, according to
+        ///    1. when string str equals to a null
+        ///    str == null;
+        ///    2. when the length of string str is ZERO 
+        ///    str.length == 0;
         /// </summary>
-        private bool tof;
+        private bool tof; // create a true or false flag
 
-        // Use if-else clause 
+        // Start a if-else clause 
         public bool isNullOrEmpty(string s) 
         {
+            // when string equals to null, returns true
             if (s == null)
             {
                 Console.Write("Input string is null, ");
                 tof = true;
             }
+            // if string is not null, then if its length is 0, returns true  
             else if (s.Length == 0)
             {
                 Console.Write("Input string is empty, ");
                 tof = true;
             }
+            // else if string is not a null and its length is not 0, returns false.
             else
             {
                 Console.Write("Input string is not null or empty, ");
                 tof = false;
             }
+            // return a true/ false 
             return tof;
         }
 
-        public static void Main()
-        {
-            // Prepare test inputs 
+        /// <summary>
+        /// Call method 'isNullOrEmpty' in Main() 
+        /// </summary>
+        public static void Main(){
+            // Prepare string arguments, to be tested in 'isNullOrEmpty' later 
             string s1 = null;
             string s2 = "a";
             string s3 = "";
             string s4 = "null";
 
-            // create class instance
+            // class instantiation, perpare to call method 'isNullOrEmpty'
             NullOrEmptyStrings noes = new NullOrEmptyStrings();
 
+            // sample 1, when passing in a null
             bool b1 = noes.isNullOrEmpty(s1);
             Console.WriteLine("Sample input: \"{0}\", output: {1}", s1, b1);
             Console.ReadLine();
 
+            // sample 2, when passing in string 'a' 
             bool b2 = noes.isNullOrEmpty(s2);
             Console.WriteLine("Sample input: \"{0}\", output: {1}", s2, b2);
             Console.ReadLine();
 
+            // sample 3, when passing in an empty string
             bool b3 = noes.isNullOrEmpty(s3);
             Console.WriteLine("Sample input: \"{0}\", output: {1}", s3, b3);
             Console.ReadLine();
 
+            // sample 4, when passing in string 'null'
             bool b4 = noes.isNullOrEmpty(s4);
             Console.WriteLine("Sample input: \"{0}\", output: {1}", s4, b4);
             Console.ReadLine();
--- NullOrEmptyStringTest/NullOrEmptyStringTests.cs
+++ NullOrEmptyStringTest/NullOrEmptyStringTests.cs
@@ -4,9 +4,15 @@ using NullOrEmptyString;
 
 namespace NullOrEmptyStringTest
 {
+    /// <summary>
+    /// Test Class 'NullOrEmptyStringTests'
+    /// </summary>
     [TestClass]
     public class NullOrEmptyStringTests 
     {
+        /// <summary>
+        /// The first test, when passing a null
+        /// </summary>
         [TestMethod]
         public void isNullOrEmpty_WhenStringIsNull()
         {
@@ -23,6 +29,9 @@ namespace NullOrEmptyStringTest
             Assert.AreEqual(actual,expected);
         }
 
+        /// <summary>
+        /// The second test, when passing an empty string
+        /// </summary>
         [TestMethod]
         public void isNullOrEmpty_WhenStringIsEmpty() { 
             // arrange
@@ -38,6 +47,9 @@ namespace NullOrEmptyStringTest
             Assert.AreEqual(actual, expected);
         }
 
+        /// <summary>
+        /// The third test, when passing an normal string, not null or empty
+        /// </summary>
         [TestMethod]
         public void isNullOrEmpty_WhenStringIsOther() { 
             // arrange
