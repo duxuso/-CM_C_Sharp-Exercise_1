3908e9c2f5449b0ded3972c88800c292f5e071e7
duxuso <duxuso@gmail.com>
2015-08-19 01:06:02 +1000

    Commit NullOrEmpty

---
+++ NullOrEmptyString/NullOrEmptyString.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace NullOrEmptyString
+{
+    public class NullOrEmptyStrings
+    {
+        /// <summary>
+        /// find a string is null, empty or not
+        /// </summary>
+        private bool tof;
+
+        // Use if-else clause 
+        public bool isNullOrEmpty(string s) 
+        {
+            if (s == null)
+            {
+                Console.Write("Input string is null, ");
+                tof = true;
+            }
+            else if (s.Length == 0)
+            {
+                Console.Write("Input string is empty, ");
+                tof = true;
+            }
+            else
+            {
+                Console.Write("Input string is not null or empty, ");
+                tof = false;
+            }
+            return tof;
+        }
+
+        public static void Main()
+        {
+            // Prepare test inputs 
+            string s1 = null;
+            string s2 = "a";
+            string s3 = "";
+            string s4 = "null";
+
+            // create class instance
+            NullOrEmptyStrings noes = new NullOrEmptyStrings();
+
+            bool b1 = noes.isNullOrEmpty(s1);
+            Console.WriteLine("Sample input: \"{0}\", output: {1}", s1, b1);
+            Console.ReadLine();
+
+            bool b2 = noes.isNullOrEmpty(s2);
+            Console.WriteLine("Sample input: \"{0}\", output: {1}", s2, b2);
+            Console.ReadLine();
+
+            bool b3 = noes.isNullOrEmpty(s3);
+            Console.WriteLine("Sample input: \"{0}\", output: {1}", s3, b3);
+            Console.ReadLine();
+
+            bool b4 = noes.isNullOrEmpty(s4);
+            Console.WriteLine("Sample input: \"{0}\", output: {1}", s4, b4);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NullOrEmptyStringTest/NullOrEmptyStringTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NullOrEmptyString;
+
+namespace NullOrEmptyStringTest
+{
+    [TestClass]
+    public class NullOrEmptyStringTests 
+    {
+        [TestMethod]
+        public void isNullOrEmpty_WhenStringIsNull()
+        {
+            // arrange
+            string test_null = null;
+            bool expected = true;
+
+            NullOrEmptyStrings noes = new NullOrEmptyStrings();
+
+            // act
+            bool actual = noes.isNullOrEmpty(test_null);
+                   
+            // assert            
+            Assert.AreEqual(actual,expected);
+        }
+
+        [TestMethod]
+        public void isNullOrEmpty_WhenStringIsEmpty() { 
+            // arrange
+            string test_empty = "";
+            bool expected = true;
+
+            NullOrEmptyStrings noes = new NullOrEmptyStrings();
+
+            // act
+            bool actual = noes.isNullOrEmpty(test_empty);
+
+            // assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void isNullOrEmpty_WhenStringIsOther() { 
+            // arrange
+            string test_other = "something";
+            bool expected = false;
+
+            NullOrEmptyStrings noes = new NullOrEmptyStrings();
+
+            // act
+            bool actual = noes.isNullOrEmpty(test_other);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
